7af329d275e1665ef4ac28c9f015d070fe596c5d
Mazaaj <johanelmazaa@gmail.com>
2024-07-09 10:28:13 -0400

    Updated resources

--- main.py
+++ main.py
@@ -77,13 +77,13 @@ while is_on:
     if choice == "off":
         is_on = False
     elif choice == "report":
-        print(f"Water: {resources[water]}ml.")
-        print(f"Milk: {resources[milk]}ml. ")
-        print(f"Coffee: {resources[coffee]}g. ")
+        print(f"Water: {resources["water"]}ml.")
+        print(f"Milk: {resources["milk"]}ml. ")
+        print(f"Coffee: {resources["coffee"]}g. ")
         print(f"Money: ${profit}")
     else:
         drink = MENU[choice]
         if is_resource_sufficient(drink["ingredients"]):
             payment = process_coins()
-            if is_transaction_successful(payment,drink["cost"]):
+            if is_transaction_successful(payment, drink["cost"]):
                 make_coffee(choice, drink["ingredients"])
