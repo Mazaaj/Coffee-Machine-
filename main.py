a767ff89d1e62a160d8ff26ba7239824f12a4d56
Mazaaj <johanelmazaa@gmail.com>
2024-07-09 10:04:20 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from data import MENU
+print(MENU)
+
+def is_resource_sufficient(order_ingredients):
+
+
+
+
+
+
+
+
+
+#user_choice = input("What would you like? (espresso/latte/capuccino):").lower()
+#if user_choice == "espresso":
+    #print("here is your espresso")
